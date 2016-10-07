6c5e55bc242f6bc71a8a4f0c642a0aab0b3db381
MariaLapovska <maria.lapovsky@gmail.com>
2016-10-07 13:53:51 +0300

    Create test and method to accept defined coin values

--- src/main/java/com/javalab/beveragemachine/BeverageMachine.java
+++ src/main/java/com/javalab/beveragemachine/BeverageMachine.java
@@ -4,4 +4,19 @@ package com.javalab.beveragemachine;
  * @author Mariia Lapovska
  */
 public class BeverageMachine {
+    private int[] coinValues;
+
+    public BeverageMachine(int[] coinValues) {
+        this.coinValues = coinValues;
+    }
+
+    public boolean acceptCoin(int value) {
+        for (int coinValue : coinValues) {
+            if (coinValue == value) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
--- src/test/java/com/javalab/beveragemachine/BeverageMachineTest.java
+++ src/test/java/com/javalab/beveragemachine/BeverageMachineTest.java
@@ -1,10 +1,30 @@
 package com.javalab.beveragemachine;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Mariia Lapovska
  */
 public class BeverageMachineTest {
+    private final int[] coinValues = new int[] {1, 5, 10, 25, 50};
 
+    private BeverageMachine beverageMachine;
+
+    @Before
+    public void setUp() {
+        beverageMachine = new BeverageMachine(coinValues);
+    }
+
+    @Test
+    public void canAcceptCoin() {
+        assertEquals(true, beverageMachine.acceptCoin(10));
+    }
+
+    @Test
+    public void canDeclineCoin() {
+        assertEquals(false, beverageMachine.acceptCoin(7));
+    }
 }
